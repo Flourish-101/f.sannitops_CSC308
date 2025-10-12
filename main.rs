6918cc34e3c32e3b1df0558b290898804698a50e
Flourish-101 <louritops@gmail.com>
2025-10-12 16:26:29 +0100

    Rust source dile

---
+++ main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Conversion from Celsius to Farenheit");
+    let v1 = 1;
+    println!("Celsius: {}", v1);
+    let v2 = v1*(9/5)+32;
+    println!("Farenheit: {}", v2);
+}
