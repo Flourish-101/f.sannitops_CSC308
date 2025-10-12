b8faceba9d64878bb9bdb42b41cd4ee7dc624de3
Flourish-101 <louritops@gmail.com>
2025-10-12 18:36:47 +0100

    Classwork 2

--- main.rs
+++ main.rs
@@ -1,8 +1,24 @@
 fn main() {
-    println!("Classwork 1");
-    println!("Conversion from Celsius to Farenheit");
-    let v1 = 1;
-    println!("Celsius: {}", v1);
-    let v2 = v1*(9/5)+32;
-    println!("Farenheit: {}", v2);
+    println!("Classwork 2\n");
+    let price:f64 = 5000.0;
+    let d1:f64 = 0.1*price;
+    let d2:f64 = 0.15*price;
+    let d3:f64 = 0.0;
+
+    if price>=5000.0{
+       let bill = price-d1;
+       println!("price ={}", price);
+       println!("discount = {}", d1);
+       println!("bill = {}", bill);
+    }else if price>=10000.0{
+       let bill = price-d2;
+       println!("price ={}", price);
+       println!("discount = {}", d2);
+       println!("bill = {}", bill);
+    }else{
+        let bill = price;
+        println!("price ={}", price);
+        println!("discount = {}",d3);
+        println!("bill = {}", bill);
+    }
 }
