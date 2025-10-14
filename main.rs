db3e9706bcb3b528f36acbf16e6cc53e4607584f
Flourish-101 <louritops@gmail.com>
2025-10-14 21:53:27 +0100

    Update main.rs

--- main.rs
+++ main.rs
@@ -5,15 +5,15 @@ fn main() {
     let d2:f64 = 0.15*price;
     let d3:f64 = 0.0;
 
-    if price>=5000.0{
-       let bill = price-d1;
-       println!("price ={}", price);
-       println!("discount = {}", d1);
-       println!("bill = {}", bill);
-    }else if price>=10000.0{
+    if price>=10000.0{
        let bill = price-d2;
        println!("price ={}", price);
        println!("discount = {}", d2);
+       println!("bill = {}", bill);
+    }else if price>=5000.0{
+       let bill = price-d1;
+       println!("price ={}", price);
+       println!("discount = {}", d1);
        println!("bill = {}", bill);
     }else{
         let bill = price;
