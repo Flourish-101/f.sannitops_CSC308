6b5af0432ae4694a53aee67bb28dffadf6431ecb
Flourish-101 <louritops@gmail.com>
2025-10-14 21:42:21 +0100

    Update main1.rs

--- main1.rs
+++ main1.rs
@@ -1,7 +1,8 @@
 fn main() {
+    println!("Classwork1");
     println!("Conversion from Celsius to Farenheit");
-    let v1 = 1;
+    let v1:f64 = 1.0;
     println!("Celsius: {}", v1);
-    let v2 = v1*(9/5)+32;
+    let v2:f64 = v1*(9.0/5.0)+32.0;
     println!("Farenheit: {}", v2);
 }
