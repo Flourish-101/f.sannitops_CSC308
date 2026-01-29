78d20a1a46b6f1afc6650e2a07fcc36f1a905b9f
Flourish_101 <louritops@gmail.com>
2026-01-29 02:34:51 +0100

    Ownership and Borrowing Tasks

---
+++ task_3/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+   let mut name= String::from("Firstname");
+   let returned_name = add_surname_to_firstname(&mut name);
+   println!("{returned_name}");
+}
+
+fn add_surname_to_firstname(name: &mut String) -> String{
+    name.push_str(" Lastname");
+    name.to_string()
+
+}
